d14294763b836a6af8fd03d0f5faf3583a34da6b
Gianluca Mazzilli <gianluca.mazzilli.external@enel.com>
2023-11-18 14:32:07 +0100

    initial commit

---
+++ MinsaitApp/src/main/java/com/indra/minsaitapp/MinsaitAppApplication.java
@@ -0,0 +1,13 @@
+package com.indra.minsaitapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinsaitAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinsaitAppApplication.class, args);
+	}
+
+}
